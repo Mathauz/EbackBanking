5d90bfa6cff57401b74efac346349c9d292c313e
Matheus Mattos <matheussilvamattos202@gmail.com>
2024-05-06 15:01:58 -0300

    .

---
+++ main.js
@@ -0,0 +1,42 @@
+const form = document.getElementById('form-deposito');
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
+let formEvalido = false;
+
+function validaNome(nomeCompleto) {
+const nomeComoArray = nomeCompleto.split(' ');
+return nomeComoArray.length >= 2;
+}
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+const numeroContaBeneficiario = document.getElementById('numero-conta');
+const valorDeposito = document.getElementById('valor-deposito');
+const mensagemSucesso = `Depósito efetuado com sucesso! R$ <b>${valorDeposito.value},00</b> - para o cliente <b>${nomeBeneficiario.value}</b> - na conta: <b>${numeroContaBeneficiario.value}</b>`;
+
+formEvalido = validaNome(nomeBeneficiario.value)
+if (formEvalido) {
+    const containerMensagemSucesso = document.querySelector('.success-message');
+    containerMensagemSucesso.innerHTML = mensagemSucesso;
+    containerMensagemSucesso.style.display = 'block';
+
+    nomeBeneficiario.value = '';
+    numeroContaBeneficiario.value = '';
+    valorDeposito.value = '';
+} else {
+    nomeBeneficiario.style.border = '1px solid red'
+    document.querySelector('.error-message').style.display = 'block';
+}
+})
+
+nomeBeneficiario.addEventListener('keyup', function(e) { console.log(e.target.value);
+formEvalido = validaNome(e.target.value);
+
+if (!formEvalido) {
+    nomeBeneficiario.classList.add('error');
+    document.querySelector('.error-message').style.display = 'block';
+} else {
+    nomeBeneficiario.classList.remove('error');
+    document.querySelector('.error-message').style.display = 'none';
+}
+})
